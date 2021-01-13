3e0ea8e3dd87f02005447e8796435ffae5b9cd69
ChaosNyaruko <cabbageparadise@gmail.com>
2021-01-13 08:35:08 +0800

    perceptron init XOR

--- dl_tutorial_demo/perceptron.py
+++ dl_tutorial_demo/perceptron.py
@@ -20,12 +20,27 @@ def NAND(x1, x2):
     else:
         return 1
 
+def OR(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([1,1])
+    b = -0.5
+    tmp = np.sum(x*w) + b
+    if tmp <=0:
+        return 0
+    else:
+        return 1
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
 if __name__ == '__main__':
-    print(AND(0,1),
-        AND(1,0),
-        AND(1,1),
-        AND(0,0))
+    print(AND(0,1),AND(1,0),AND(1,1),AND(0,0))
     print(NAND(0,1),NAND(1,0),NAND(1,1),NAND(0,0))
+    print(OR(0,1),OR(1,0),OR(1,1),OR(0,0))
+    print(XOR(0,1),XOR(1,0),XOR(1,1),XOR(0,0))
 
 
 
